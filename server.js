1a2b2b5d86bde548bf95db85fedb55bb85b6a430
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-11-20 22:14:18 -0300

    refactor: distribuindo as responsabilidades das lógicas desenvolvidas no server.js para Routes, Controller e Models

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express from "express"; // Importa o framework Express para criar a aplicação web
-import conectarAoBanco from "./src/config/dbConfig.js"; // Importa a função para conectar ao banco de dados (definida em dbConfig.js)
+import routes from "./src/routes/postsRoutes.js";
 
-// Conecta ao banco de dados usando a string de conexão fornecida pela variável de ambiente STRING_CONEXAO
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 // Array de posts de exemplo (será substituído pelos dados do banco de dados)
 // const posts = [
@@ -11,31 +9,13 @@ const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 // Cria uma instância do Express
 const app = express();
-// Permite que o Express entenda requisições com formato JSON
-app.use(express.json());
+routes(app);
 
 // Inicia o servidor na porta 3000
 app.listen(3000, () => {
   console.log("servidor escutando...");
 });
 
-// Função assíncrona para obter todos os posts do banco de dados
-async function getTodosPosts() {
-  // Seleciona o banco de dados "imersao-instabytes"
-  const db = conexao.db("imersao-instabytes");
-  // Seleciona a coleção "posts" dentro do banco de dados
-  const colecao = db.collection("posts");
-  // Retorna todos os documentos da coleção como um array
-  return colecao.find().toArray();
-}
-
-// Rota GET para obter todos os posts
-app.get("/posts", async (req, res) => {
-  // Chama a função para obter os posts do banco de dados
-  const resultadoPosts = await getTodosPosts();
-  // Envia os posts como resposta em formato JSON com status 200 (sucesso)
-  res.status(200).json(posts);
-});
 // app.get("/posts", (req, res) => {
 //     res.status(200).json(posts);
 // });
