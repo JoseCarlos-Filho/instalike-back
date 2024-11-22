7ff2df0ba9389723358ff26f5ec63087db7b0d89
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-11-21 21:36:25 -0300

    feat: função chamada do controller que cria um novo post acessando o banco e a coleção a ser criado o post, usando a função insertOne(novoPost), esta função esta na documentação do MongoDB.

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -4,11 +4,17 @@ import conectarAoBanco from "../config/dbConfig.js"; // Importa a função para 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 // Função assíncrona para obter todos os posts do banco de dados
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
     // Seleciona o banco de dados "imersao-instabytes"
     const db = conexao.db("imersao-instabytes");
     // Seleciona a coleção "posts" dentro do banco de dados
     const colecao = db.collection("posts");
     // Retorna todos os documentos da coleção como um array
     return colecao.find().toArray();
+  };
+
+  export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
   }
