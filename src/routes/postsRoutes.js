260e2fdb6c773e5276bc2ee56e8fdff5d03d07be
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-11-21 21:31:32 -0300

    feat: criando uma nova rota para post. postando um conteudo novo no banco de dados.

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
 
 const routes = (app) => {
     // Permite que o Express entenda requisições com formato JSON
@@ -7,6 +7,11 @@ const routes = (app) => {
 
     // Rota GET para obter todos os posts
     app.get("/posts", listarPosts);
+    // Rota para criar um post
+    app.post("/posts", postarNovoPost);
 }
 
 export default routes;
+
+
+
