c9fd19e149584d19a760e92f226098cae77a4a72
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-11-25 22:36:13 -0300

    feat: import do multer, cors, função para controle de sistema de arquivos no windows utilizando multer.

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,30 @@
 import express from "express";
-import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
+import cors from "cors";
 
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200 
+}
+
+// código receita de bolo para configurar o sistema de arquivos ao enviar para o banco de dados.
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage});
+// linux ou mac utilizar somente o seguinte código
+// const upload = multer({ dest: "./uploads" });
 const routes = (app) => {
     // Permite que o Express entenda requisições com formato JSON
     app.use(express.json());
+    app.use(cors(corsOptions));
 
     // Rota GET para obter todos os posts
     app.get("/posts", listarPosts);
