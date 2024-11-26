b061c65b0776639e9dfa3797e7460352b6b29acf
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-11-25 22:39:38 -0300

    feat: atualização de imports, criação das funções - uploadImagem(), atualizarNovoPost() e chamada da função importada atualizarPost()

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,6 @@
-import { getTodosPosts, criarPost } from "../models/postsModel.js";
+import fs from "fs";
+import { getTodosPosts, criarPost, atualizarPost } from "../models/postsModel.js";
+import gerarDescricaoComGemini from "../services/geminiServices.js";
 
 export async function listarPosts (req, res) {
     // Chama a função para obter os posts do banco de dados
@@ -16,5 +18,45 @@ export async function postarNovoPost(req, res) {
         console.error(erro.message);
         res.status(500).json({"Erro" : "Falha na requisição"});
     }
-}
+};
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgURL: req.file.originalname,
+        alt: ""
+    };
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.jpg`;
+        fs.renameSync(req.file.path, imagemAtualizada);
+        res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro" : "Falha na requisição"});
+    }
+};
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `http://localhost:3000/${id}.jpg`;
+    
+    try {
+        const imgBuffer = fs.readFileSync(`uploads/${id}.jpg`);
+        const descricao = await gerarDescricaoComGemini(imgBuffer);
+
+        const post = {
+            imgURL: urlImagem,
+            descricao: descricao,
+            alt: req.body.alt
+        }
+
+        const postCriado = await atualizarPost(id, post);
+        res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro" : "Falha na requisição"});
+    }
+};
 
